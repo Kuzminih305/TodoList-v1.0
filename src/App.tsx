3024bfdc02083061e4695ca72e517daef148e17b
Kuzminih305 <Kuzminih187@gmail.com>
2023-04-10 13:14:36 +0300

    TodoList v1.0 Redux

--- src/App.tsx
+++ src/App.tsx
@@ -39,59 +39,6 @@ export type TasksStateType = {
 }
 
 function App() {
-    // let [tasks, setTasks] = useState([
-    //     {id: v1(), title: "HTML&CSS", isDone: true},
-    //     {id: v1(), title: "JS", isDone: true},
-    //     {id: v1(), title: "ReactJS", isDone: false},
-    //     {id: v1(), title: "Rest API", isDone: false},
-    //     {id: v1(), title: "GraphQL", isDone: false},
-    // ]);
-    // let [filter, setFilter] = useState<FilterValuesType>("all");
-    // let [todolists, setTodolists] = useState<Array<TodolistsType>>([
-    //     {id: todolistID1, title: 'What to learn', filter: 'all'},
-    //     {id: todolistID2, title: 'What to buy', filter: 'all'},
-    // ])
-    // let [tasks, setTasks] = useState<TasksStateType>({
-    //     [todolistID1]: [
-    //         {id: v1(), title: "HTML&CSS", isDone: true},
-    //         {id: v1(), title: "JS", isDone: true},
-    //         {id: v1(), title: "ReactJS", isDone: false},
-    //         {id: v1(), title: "Rest API", isDone: false},
-    //         {id: v1(), title: "GraphQL", isDone: false},
-    //     ],
-    //     [todolistID2]: [
-    //         {id: v1(), title: "HTML&CSS2", isDone: true},
-    //         {id: v1(), title: "JS2", isDone: true},
-    //         {id: v1(), title: "ReactJS2", isDone: false},
-    //         {id: v1(), title: "Rest API2", isDone: false},
-    //         {id: v1(), title: "GraphQL2", isDone: false},
-    //     ]
-    // });
-
-    // let todolistID1 = v1();
-    // let todolistID2 = v1();
-    //
-    // const [todolists, todolistsDispatch] = useReducer(todolistsReducer, [
-    //     {id: todolistID1, title: 'What to learn', filter: 'all'},
-    //     {id: todolistID2, title: 'What to buy', filter: 'all'},
-    // ])
-    //
-    // const [tasks, tasksDispatch] = useReducer(tasksReducer, {
-    //         [todolistID1]: [
-    //             {id: v1(), title: "HTML&CSS", isDone: true},
-    //             {id: v1(), title: "JS", isDone: true},
-    //             {id: v1(), title: "ReactJS", isDone: false},
-    //             {id: v1(), title: "Rest API", isDone: false},
-    //             {id: v1(), title: "GraphQL", isDone: false},
-    //         ],
-    //         [todolistID2]: [
-    //             {id: v1(), title: "HTML&CSS2", isDone: true},
-    //             {id: v1(), title: "JS2", isDone: true},
-    //             {id: v1(), title: "ReactJS2", isDone: false},
-    //             {id: v1(), title: "Rest API2", isDone: false},
-    //             {id: v1(), title: "GraphQL2", isDone: false},
-    //         ]
-    // });
 
     const todoLists = useSelector<AppRootStateType, Array<TodolistsType>>(state => state.todoLists)
     const tasks = useSelector<AppRootStateType, TasksStateType>(state => state.tasks)
